ad4c8aa0f02d73f6065f5f4e3ef5ee7870bd6d80
DANILO LOPES <99082226+daniloalveslopes@users.noreply.github.com>
2023-10-02 13:14:57 -0300

    resolvi um bug nos links de nav, bug de scroll

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let nav = document.querySelector(".nav");
+let header = document.querySelector("header")
 let quemBtn = document.querySelector("#quemLink")
 let depoimentosBtn = document.querySelector("#depoimentosLink")
 let contatosBtn = document.querySelector("#contatosLink")
@@ -8,16 +9,20 @@ let btnNavOpen = document.getElementById('btnNavOpen')
 btnNavOpen.addEventListener('click', () => {
     nav.classList.toggle('show');
     html.classList.toggle('scroll')
+    console.log(header.clientWidth);
 })
 quemBtn.addEventListener('click', () => {
     nav.classList.toggle('show');
+    if(header.clientWidth < 650)
     html.classList.toggle('scroll')
 })
 depoimentosBtn.addEventListener('click', () => {
     nav.classList.toggle('show');
+    if(header.clientWidth < 650)
     html.classList.toggle('scroll')
 })
 contatosBtn.addEventListener('click', () => {
     nav.classList.toggle('show');
+    if(header.clientWidth < 650)
     html.classList.toggle('scroll')
 })
